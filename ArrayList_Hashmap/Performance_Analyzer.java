7d41ee2a36a8b0a6f3f7e68ba2c372837c34b5a3
anjupravasahoo88-code <anjupravasahoo88@gmail.com>
2026-06-09 17:58:24 +0530

    Update Performance_Analyzer.java

--- ArrayList_Hashmap/Performance_Analyzer.java
+++ ArrayList_Hashmap/Performance_Analyzer.java
@@ -1,27 +1,66 @@
-/*
-Create a Java program to manage student course performance.
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-Task:
+class Student {
+    String studentName;
+    String courseName;
+    int marks;
 
-You are given multiple students with the following details:
+    Student(String studentName, String courseName, int marks) {
+        this.studentName = studentName;
+        this.courseName = courseName;
+        this.marks = marks;
+    }
+}
+public class CoursePerformance {
+	public static void main(String[] args) {
 
-Student Name
-Course Name
-Marks
-Requirements:
-Store all student records using ArrayList
-Use a HashMap<String, List<Integer>> where:
-Key = Course Name
-Value = List of marks in that course
-Calculate and display:
-Average marks for each course
-Course with highest average
-Hint:
+       
+        ArrayList<Student> students = new ArrayList<>();
 
-Break the problem into steps:
+        students.add(new Student("Rahul", "Java", 85));
+        students.add(new Student("Priya", "Java", 90));
+        students.add(new Student("Amit", "Python", 78));
+        students.add(new Student("Sneha", "Python", 88));
+        students.add(new Student("Riya", "C++", 92));
+        students.add(new Student("Karan", "C++", 86));
 
-First store raw data in ArrayList
-Then group marks by course using HashMap
-Then iterate HashMap to calculate averages
-Finally compare averages
-  */
+        
+        HashMap<String, List<Integer>> courseMarks = new HashMap<>();
+
+        
+        for (Student s : students) {
+            courseMarks.putIfAbsent(s.courseName, new ArrayList<>());
+            courseMarks.get(s.courseName).add(s.marks);
+        }
+
+       
+        String highestCourse = "";
+        double highestAverage = 0;
+
+        System.out.println("Average Marks for Each Course:");
+
+        for (String course : courseMarks.keySet()) {
+            List<Integer> marksList = courseMarks.get(course);
+
+            int sum = 0;
+            for (int mark : marksList) {
+                sum += mark;
+            }
+
+            double average = (double) sum / marksList.size();
+
+            System.out.println(course + " = " + average);
+
+            if (average > highestAverage) {
+                highestAverage = average;
+                highestCourse = course;
+            }
+        }
+
+       
+        System.out.println("\nCourse with Highest Average:");
+        System.out.println(highestCourse + " = " + highestAverage);
+    }
+}
